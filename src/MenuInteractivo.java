20ea6062b51d57e17b9976646559ba739cb8ce10
alejandro891 <yalazarot@ufpso.edu.co>
2025-03-07 17:51:46 -0500

    Menu

--- src/MenuInteractivo.java
+++ src/MenuInteractivo.java
@@ -1,56 +1,40 @@
 import java.util.Scanner;
 
 public class MenuInteractivo {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("1.sumar");
+        System.out.println("2.restar");
+        System.out.println("3.multiplicar");
+        System.out.println("4.division");
 
-        // pedimos al usuario dos nuemros
-        System.out.println("ingresa dos nuemros para hacer una operacion matematica");
-        System.out.println("ingresa el primer numero");
+        int opcion= scanner.nextInt();
+
+        System.out.print("Ingrese el primer número: ");
         int num1 = scanner.nextInt();
-        System.out.println("ingresa el segundo numero");
+        System.out.print("Ingrese el segundo número: ");
         int num2 = scanner.nextInt();
 
-        //pedimos al usuario que operacion quiere hacer
-        System.out.println("ingresa 1 para sumar");
-        System.out.println("ingresa 2 para restar");
-        System.out.println("ingresa 3 para multiplicar");
-        System.out.println("ingresa 4 para division");
-        int num = scanner.nextInt();
-        
-        // evaluamos la opcion dada por el usuario y hacemos las respectivas operaciones
-        // suma
-        if (num==1) { 
-            int numSuma = num1+num2;
-            System.out.println("la suma es: " + numSuma);
-            
-        } else {
-            //resta
-            if (num==2) {
-                int numResta = num1-num2;
-                System.out.println("la resta es: " + numResta);
-                
-            } else {
-                //multiplicacion
-                if (num==3) {
-                    int numMultiplicacion = num1*num2;
-                    System.out.println("la multiplicacion es: " + numMultiplicacion);
-                    
-                } else {
-                    //division
-                    if (num==4) {
-                        int numDivision = num1 / num2;
-                        System.out.println("la division es: " + numDivision);
-                        
-                    } else { 
-                        System.out.println("opcion incorrecta");
-                        
-                    }
-                }
-                
+        switch (opcion) {
+            case 1:
+                System.out.println("suma: " + num1+num2);
+                break;
+            case 2:
+                System.out.println("resta:" + (num1-num2));
+                break;
+            case 3:
+                System.out.println("multiplicacion: " + num1*num2);
+                break;
+            case 4:
+                System.out.println("division: " + num1/num2);
+                break;
+            default:
+                System.out.println("Opción no válida.");
+                break;
             }
-            
         }
+        
+
+        
     }
-    
-}
