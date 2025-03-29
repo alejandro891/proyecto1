f85b156c34af395ed7c58e29e6ffeb4d7b78ec86
alejandro <yalazarot@ufpso.edu.co>
2025-03-29 07:53:56 -0500

    ejercicios

--- src/NumerosAmigos.java
+++ src/NumerosAmigos.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ValidadorNumerosAmigos {
+public class NumerosAmigos {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
